ca6aac9f0c9119b95fd620ac90d108dea69724cb
Nataliya Stepanova <gnykka@gmail.com>
2021-06-18 11:38:37 +0300

    Add zoom gesture

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -17,7 +17,7 @@ $(document).ready(() => {
 
   const center = { x: -1000, y: 0 };
 
-  const visibleRegion = new VisibleRegion2d(center.x, center.y, 10);
+  const visibleRegion = new VisibleRegion2d(center.x, center.y, 1000000);
   const viewport = new Viewport2d(1, $vc[0].clientWidth, $vc[0].clientHeight, visibleRegion);
 
   const updateAxis = () => {
@@ -31,24 +31,38 @@ $(document).ready(() => {
 
   const canvasGestures = Gestures.getGesturesStream($vc);
 
-  // TODO: обрабатывать touch жесты и zoom
+  // TODO: обрабатывать touch жесты
   // TODO: обрабатывать жесты на оси
   // TODO: починить переключение оси на другой режим
   // TODO: включить плавные жесты через viewport-controller
-
+  // TODO: ограничивать глубину зума
   // TODO: обновлять маркер текущего значения
 
-  canvasGestures.subscribe(val => {
-    if (val.Type === 'Pan') {
-      const virtualOffset = viewport.vectorScreenToVirtual(val.xOffset, val.yOffset);
+  canvasGestures.subscribe(value => {
+    if (value.Type === 'Pan') {
+      const virtualOffset = viewport.vectorScreenToVirtual(value.xOffset, value.yOffset);
 
       viewport.visible.centerX = center.x - virtualOffset.x;
       viewport.visible.centerY = center.y - virtualOffset.y;
 
       updateAxis();
-    } else if (val.Type === 'PanEnd') {
+    } else if (value.Type === 'PanEnd') {
       center.x = viewport.visible.centerX;
       center.y = viewport.visible.centerY;
+    } else if (value.Type === 'Zoom') {
+      const x = value.xOrigin + (viewport.width / 2.0 - value.xOrigin) * value.scaleFactor;
+      const y = value.yOrigin + (viewport.height / 2.0 - value.yOrigin) * value.scaleFactor;
+
+      const newCenter = viewport.pointScreenToVirtual(x, y);
+
+      center.x = newCenter.x;
+      center.y = newCenter.y;
+
+      viewport.visible.centerX = newCenter.x;
+      viewport.visible.centerY = newCenter.y;
+      viewport.visible.scale *= value.scaleFactor;
+
+      updateAxis();
     }
   });
 });
